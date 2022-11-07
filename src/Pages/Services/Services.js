2ef85fae6fa28d171583f4e56f1e6e8d04525e16
muttakin1991 <shawonmuttakin@gmail.com>
2022-11-08 02:22:44 +0600

    extra section done

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,8 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import image1 from '../../assets/images/package-1.jpg';
-import image2 from '../../assets/images/package-2.jpg';
-import image3 from '../../assets/images/package-3.jpg';
+import { faCalendarDay, faDollar, faLocationPin, faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Services = () => {
     return (
@@ -10,34 +10,99 @@ const Services = () => {
             <h1 className='text-center p-5'>My Services</h1>
             <div className="row">
                 <div className="col-lg-4">
-                    <div class="card">
-                        <img src={image1} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <Link to="#" class="btn btn-primary">Go somewhere</Link>
+                    <div className="card">
+                        <img src={image1} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center pb-2 text-primary">Card title</h5>
+                            <div className="d-flex justify-content-between align-items-center pt-2 pb-2">
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faLocationPin}></FontAwesomeIcon>
+                                    <p className='m-0'>cox's bazar</p>
+                                </div>
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faCalendarDay}></FontAwesomeIcon>
+                                    <p className='p-0 m-0'>cox's bazar</p>
+                                </div>
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faDollar}></FontAwesomeIcon>
+                                    <p className='p-0 m-0'>cox's bazar</p>
+                                </div>
+                            </div>
+                            <p className="mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr />
+                            <div className='d-flex align-items-center justify-content-between'>
+                                <Link to="#" className="btn btn-success">Show Details</Link>
+                                <div>
+                                    <FontAwesomeIcon icon={faStar} className='text-warning'></FontAwesomeIcon><span>(4)</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <Link to="#" class="btn btn-primary">Go somewhere</Link>
+                    <div className="card">
+                        <img src={image1} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center pb-2 text-primary">Card title</h5>
+                            <div className="d-flex justify-content-between align-items-center pt-2 pb-2">
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faLocationPin}></FontAwesomeIcon>
+                                    <p className='m-0'>cox's bazar</p>
+                                </div>
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faCalendarDay}></FontAwesomeIcon>
+                                    <p className='p-0 m-0'>cox's bazar</p>
+                                </div>
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faDollar}></FontAwesomeIcon>
+                                    <p className='p-0 m-0'>cox's bazar</p>
+                                </div>
+                            </div>
+                            <p className="mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr />
+                            <div className='d-flex align-items-center justify-content-between'>
+                                <Link to="#" className="btn btn-success">Show Details</Link>
+                                <div>
+                                    <FontAwesomeIcon icon={faStar} className='text-warning'></FontAwesomeIcon><span>(4)</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+               
                 <div className="col-lg-4">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <Link to="#" class="btn btn-primary">Go somewhere</Link>
+                    <div className="card">
+                        <img src={image1} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center pb-2 text-primary">Card title</h5>
+                            <div className="d-flex justify-content-between align-items-center pt-2 pb-2">
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faLocationPin}></FontAwesomeIcon>
+                                    <p className='m-0'>cox's bazar</p>
+                                </div>
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faCalendarDay}></FontAwesomeIcon>
+                                    <p className='p-0 m-0'>cox's bazar</p>
+                                </div>
+                                <div className="text-center">
+                                    <FontAwesomeIcon className='text-primary' icon={faDollar}></FontAwesomeIcon>
+                                    <p className='p-0 m-0'>cox's bazar</p>
+                                </div>
+                            </div>
+                            <p className="mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr />
+                            <div className='d-flex align-items-center justify-content-between'>
+                                <Link to="#" className="btn btn-success">Show Details</Link>
+                                <div>
+                                    <FontAwesomeIcon icon={faStar} className='text-warning'></FontAwesomeIcon><span>(4)</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className='text-center mt-4 mb-4'>
+                    <button className='btn btn-outline-success w-25 m-auto'>More Services</button>
+
                 </div>
             </div>
         </div>
